dfb263b1162204814fdd5f9d36ea06f0f22630c8
Jakob Schuppan <jakob.schuppan@googlemail.com>
2019-02-21 20:24:59 -0500

    new stuff

---
+++ scheduler.cpp
@@ -0,0 +1,17 @@
+#include <pthread.h>
+#include <list>
+#include "scheduler.h"
+
+void Scheduler::create_task(functionPtr, threadArg, threadName) {
+  
+}
+
+void Scheduler::yield() {
+  int result;
+  //result = callingThread.pthread_yield();
+
+  if (result != 0) {
+    perror("Fatal error: pThread failed to yield!");
+    exit(0);
+  }
+}
