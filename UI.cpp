1f4539046af9270e6476e91b96ae215106244440
Robert <rd6@iu.edu>
2019-02-22 00:00:24 -0500

    jj

--- UI.cpp
+++ UI.cpp
@@ -5,20 +5,18 @@
 UI :: UI()
 {
   getmaxyx(stdscr, maxY, maxX); // get console screen size
-  head = NULL;
   wprintw(stdscr, "Screen Height = %d, Screen width = %d", maxY, maxX);
   refresh();
-  windowCount = 0;
   addStarterWindows();
 }
 
 //adds UI windows
-void UI :: addStarterWindows() 
+void UI :: addStarterWindows()
 {
   //can only create windows if no windows exist
   if(head == NULL)
-    { 
-    
+    {
+
     //Header Window
       head = new windowNode;
       head->next = NULL;
@@ -26,7 +24,7 @@ void UI :: addStarterWindows()
       windowNode* temp = head;
       windowCount++;
       head->Win->write_window("Header");
-        
+
      //Control window
      temp->next = new windowNode;
      int y2 = temp->Win->getY() + temp->Win->getHeight();
@@ -34,10 +32,10 @@ void UI :: addStarterWindows()
      temp = temp->next;
      temp->next = NULL;
      windowCount++;
-     
+
      int tempx = temp->Win->getX() + temp->Win->getWidth();
      int tempy = temp->Win->getY();
-     
+
      //Input window
      temp->next = new windowNode;
      tempx = temp->Win->getX() + temp->Win->getWidth();
@@ -62,7 +60,7 @@ void UI :: addNewWindow()
      {
         temp = temp->next;
      }
-     
+
      if(windowCount >= 9)
         return;
      if(windowCount == 3)
@@ -73,7 +71,7 @@ void UI :: addNewWindow()
          temp->next->Win = new Window(height, width, y2, 2);
          temp = temp->next;
          temp->next = NULL;
-         
+
          head->next->Win->moveWindow(temp->Win->getY()+temp->Win->getHeight(), 2);
          head->next->next->Win->moveWindow(temp->Win->getY()+temp->Win->getHeight(), head->next->Win->getX() + head->next->Win->getWidth());
          update();
@@ -136,10 +134,10 @@ void UI :: update()
     wclear(stdscr);
     wprintw(stdscr, "Screen Height = %d, Screen width = %d", maxY, maxX);
     wrefresh(stdscr);
-    
+
     windowNode* temp = head;
     while(temp)
-    {    
+    {
         if(temp == NULL)
             continue;
         temp->Win->windowRefresh();
