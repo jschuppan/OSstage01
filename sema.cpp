a79109a1b061947d1230da93eca28aaf1b7b3614
Jakob Schuppan <jakob.schuppan@googlemail.com>
2019-02-21 23:57:47 -0500

    sema

--- sema.cpp
+++ sema.cpp
@@ -1,9 +1,19 @@
 #include "sema.h"
 
+
 Semaphore::Semaphore(std::string resName) {
   this->resName = resName;
   this->sema_value = 1;
 }
 
-void Semaphore::down(int threadID);
-void Semaphore::up();
+void Semaphore::down(int threadID) {
+  // case 1: resource is available
+  if (sema_value == 1) {
+
+  }
+  // case 2: resource is unavailable
+  
+}
+void Semaphore::up() {
+
+}
