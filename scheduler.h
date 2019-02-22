52706005895ecf3569e7e9c8dce9d547c112da67
Jakob Schuppan <jakob.schuppan@googlemail.com>
2019-02-21 19:04:47 -0500

    cc

--- scheduler.h
+++ scheduler.h
@@ -12,11 +12,11 @@ class Scheduler {
     pthread_t[MAX_WINDOWS_THREADS];
 
   public:
-    bool create_task(functionPtr, threadArg, threadName);   // create appropriate data structures and calls coroutine()
-    bool destroy_task();  // to kill a task (Set its status to DEAD)
-    bool yield();  // strict round robin process switch.
-    bool dump(int level);
-    bool garbage_collect();
+    void create_task(functionPtr, threadArg, threadName);   // create appropriate data structures and calls coroutine()
+    void destroy_task();  // to kill a task (Set its status to DEAD)
+    void yield();  // strict round robin process switch.
+    void dump(int level);
+    void garbage_collect();
 
 };
 
