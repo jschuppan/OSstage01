33b6cafb3d33aae5b090cd24371b4c9bc04df73b
Jakob Schuppan <jakob.schuppan@googlemail.com>
2019-03-17 01:09:15 -0400

    fixed

--- IPC.h
+++ IPC.h
@@ -4,13 +4,10 @@
 #include <string>
 #include <iostream>
 #include "ezQueue.h"
-<<<<<<< HEAD
 #include <list>
 #include <queue>
 #include "MCB.h"
-=======
 #include "linkedlist.h"
->>>>>>> 4b91861decfd32652abc844b4da64aa93079de66
 
 class IPC
 {
